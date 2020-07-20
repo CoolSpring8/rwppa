a04d15a47141a52b02223671bac7b5f79f0da22a
CoolSpring8 <coolspring888@gmail.com>
2020-07-20 16:39:57 +0800

    style: more readable format and getting ready for pubic access

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -33,20 +33,20 @@ import (
 )
 
 var (
-	// rvpnURLMatcher detects RVPN-modified URLs.
-	rvpnURLMatcher *regexp.Regexp = regexp.MustCompile(`/web/[0-3]/(https?)/[0-2]/`)
+	// RVPNURLMatcher detects RVPN-modified URLs.
+	RVPNURLMatcher *regexp.Regexp = regexp.MustCompile(`/web/[0-3]/(https?)/[0-2]/`)
 
-	// movedLocationURLMatcher detects RVPN-modified URL in the 3xx response's Location header.
-	movedLocationURLMatcher *regexp.Regexp = regexp.MustCompile(`https://.*:443/web/[0-3]/(https?)/[0-2]/`)
+	// MovedLocationURLMatcher detects RVPN-modified URL in the 3xx response's Location header.
+	MovedLocationURLMatcher *regexp.Regexp = regexp.MustCompile(`https://.*:443/web/[0-3]/(https?)/[0-2]/`)
 
-	// isOPTIONSRequest checks whether the request's method is OPTIONS.
-	isOPTIONSRequest goproxy.ReqCondition = goproxy.ReqConditionFunc(
+	// IsOPTIONSRequest checks whether the request's method is OPTIONS.
+	IsOPTIONSRequest goproxy.ReqCondition = goproxy.ReqConditionFunc(
 		func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
 			return req.Method == "OPTIONS"
 		})
 
-	// hasMovedLocationHeader checks whether the request has a Location header.
-	hasMovedLocationHeader goproxy.RespCondition = goproxy.RespConditionFunc(
+	// HasMovedLocationHeader checks whether the request has a Location header.
+	HasMovedLocationHeader goproxy.RespCondition = goproxy.RespConditionFunc(
 		func(resp *http.Response, ctx *goproxy.ProxyCtx) bool {
 			if resp == nil {
 				return false
@@ -54,8 +54,8 @@ var (
 			return resp.Header.Get("Location") != ""
 		})
 
-	// isWebRelatedText checks whether the response's content type is one of html, js, css, xml and json.
-	isWebRelatedText goproxy.RespCondition = goproxy.ContentTypeIs(
+	// IsWebRelatedText checks whether the response's content type is one of html, js, css, xml and json.
+	IsWebRelatedText goproxy.RespCondition = goproxy.ContentTypeIs(
 		"text/html",
 		"text/css",
 		"text/javascript", "application/javascript", "application/x-javascript",
@@ -86,66 +86,11 @@ func StartProxyServer(listenAddr, twfid string) {
 	proxy.Verbose = true
 
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-	proxy.OnRequest().DoFunc(
-		func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			// store rawURL for later use
-			rawURLWithPort := req.URL.String()
-			hostWithoutPort, _, err := net.SplitHostPort(req.URL.Host)
-			if err != nil {
-				hostWithoutPort = req.URL.Host // http at port 80 causes "missing port in address"
-			}
-			rawURLWithoutPort := req.URL.Scheme + "://" + hostWithoutPort + req.URL.Path // Query isn't here, but it doesn't matter
-			ctx.UserData = reqData{rawURLWithPort, rawURLWithoutPort}
+	proxy.OnRequest().Do(goproxy.FuncReqHandler(SendToRVPNHandler(twfid)))
+	proxy.OnRequest(IsOPTIONSRequest).DoFunc(OPTIONSRequestHandler)
 
-			// new request target
-			// TODO: add other missed fields in "type URL struct" if necessary, like "User"
-			// port has been included in "Host"
-			if req.URL.RawQuery != "" {
-				newURL, err := url.Parse("https://rvpn.zju.edu.cn/web/2/" + req.URL.Scheme + "/0/" + req.URL.Host + req.URL.Path + "?" + req.URL.RawQuery)
-				if err != nil {
-					return req, nil // this rarely happens?
-				}
-				req.URL = newURL
-			} else {
-				newURL, err := url.Parse("https://rvpn.zju.edu.cn/web/2/" + req.URL.Scheme + "/0/" + req.URL.Host + req.URL.Path)
-				if err != nil {
-					return req, nil
-				}
-				req.URL = newURL
-			}
-
-			// add cookie for web portal verification
-			req.AddCookie(&http.Cookie{Name: "TWFID", Value: twfid})
-
-			return req, nil
-		})
-	proxy.OnRequest(isOPTIONSRequest).DoFunc(
-		func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			resp := goproxy.NewResponse(req, goproxy.ContentTypeText, http.StatusOK, "")
-			resp.Header.Add("Access-Control-Allow-Credentials", "true")
-			resp.Header.Add("Access-Control-Allow-Headers", "authorization, content-type")
-			resp.Header.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS")
-			resp.Header.Add("Access-Control-Allow-Origin", "*")
-			return req, resp
-		})
-
-	proxy.OnResponse(hasMovedLocationHeader).DoFunc(
-		func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-			respLocation := resp.Header.Get("Location")
-			newLocation := movedLocationURLMatcher.ReplaceAllString(respLocation, "$1://")
-			resp.Header.Set("Location", newLocation)
-			return resp
-		})
-	proxy.OnResponse(isWebRelatedText).Do(goproxy_html.HandleString(
-		func(s string, ctx *goproxy.ProxyCtx) string {
-			// fix links in page, and fix "src" issues in javascript files
-			c := rvpnURLMatcher.ReplaceAllString(s, "$1://")
-			rawURLWithPort := ctx.UserData.(reqData).rawURLWithPort
-			rawURLWithoutPort := ctx.UserData.(reqData).rawURLWithoutPort
-			c = strings.ReplaceAll(c, rawURLWithPort[:strings.LastIndex(rawURLWithPort, "/")+1], "") // possible out of bounds?
-			c = strings.ReplaceAll(c, rawURLWithoutPort[:strings.LastIndex(rawURLWithoutPort, "/")+1], "")
-			return c
-		}))
+	proxy.OnResponse(HasMovedLocationHeader).DoFunc(MovedLocationHandler)
+	proxy.OnResponse(IsWebRelatedText).Do(WebTextHandler())
 
 	fmt.Println("Current TWFID:" + twfid)
 	fmt.Println("Listen Address:" + listenAddr)
@@ -168,4 +113,71 @@ func SetCA(caCert, caKey []byte) error {
 	goproxy.HTTPMitmConnect = &goproxy.ConnectAction{Action: goproxy.ConnectHTTPMitm, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
 	goproxy.RejectConnect = &goproxy.ConnectAction{Action: goproxy.ConnectReject, TLSConfig: goproxy.TLSConfigFromCA(&goproxyCa)}
 	return nil
+}
+
+// SendToRVPNHandler sends the original request to RVPN web portal.
+func SendToRVPNHandler(twfid string) func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	return func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		// store rawURL for later use
+		rawURLWithPort := req.URL.String()
+		hostWithoutPort, _, err := net.SplitHostPort(req.URL.Host)
+		if err != nil {
+			hostWithoutPort = req.URL.Host // http at port 80 causes "missing port in address"
+		}
+		rawURLWithoutPort := req.URL.Scheme + "://" + hostWithoutPort + req.URL.Path // Query isn't here, but it doesn't matter
+		ctx.UserData = reqData{rawURLWithPort, rawURLWithoutPort}
+
+		// new request target
+		// TODO: add other missed fields in "type URL struct" if necessary, like "User"
+		// port has been included in "Host"
+		if req.URL.RawQuery != "" {
+			newURL, err := url.Parse("https://rvpn.zju.edu.cn/web/2/" + req.URL.Scheme + "/0/" + req.URL.Host + req.URL.Path + "?" + req.URL.RawQuery)
+			if err != nil {
+				return req, nil // this rarely happens?
+			}
+			req.URL = newURL
+		} else {
+			newURL, err := url.Parse("https://rvpn.zju.edu.cn/web/2/" + req.URL.Scheme + "/0/" + req.URL.Host + req.URL.Path)
+			if err != nil {
+				return req, nil
+			}
+			req.URL = newURL
+		}
+
+		// add cookie for web portal verification
+		req.AddCookie(&http.Cookie{Name: "TWFID", Value: twfid})
+
+		return req, nil
+	}
+}
+
+// OPTIONSRequestHandler skips sending the request and returns a response designed for CORS preflight request.
+func OPTIONSRequestHandler(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	resp := goproxy.NewResponse(req, goproxy.ContentTypeText, http.StatusOK, "")
+	resp.Header.Add("Access-Control-Allow-Credentials", "true")
+	resp.Header.Add("Access-Control-Allow-Headers", "authorization, content-type")
+	resp.Header.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS")
+	resp.Header.Add("Access-Control-Allow-Origin", "*")
+	return req, resp
+}
+
+// MovedLocationHandler fixes redirections.
+func MovedLocationHandler(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	respLocation := resp.Header.Get("Location")
+	newLocation := MovedLocationURLMatcher.ReplaceAllString(respLocation, "$1://")
+	resp.Header.Set("Location", newLocation)
+	return resp
+}
+
+// WebTextHandler fixes links in page and "src" issues in javascript files.
+func WebTextHandler() goproxy.RespHandler {
+	return goproxy_html.HandleString(
+		func(s string, ctx *goproxy.ProxyCtx) string {
+			c := RVPNURLMatcher.ReplaceAllString(s, "$1://")
+			rawURLWithPort := ctx.UserData.(reqData).rawURLWithPort
+			rawURLWithoutPort := ctx.UserData.(reqData).rawURLWithoutPort
+			c = strings.ReplaceAll(c, rawURLWithPort[:strings.LastIndex(rawURLWithPort, "/")+1], "") // possible out of bounds?
+			c = strings.ReplaceAll(c, rawURLWithoutPort[:strings.LastIndex(rawURLWithoutPort, "/")+1], "")
+			return c
+		})
 }
